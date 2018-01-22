068ad8ca75f01e9f669979cf1954334aa4dd5e36
Mary Chew <jiayi1172@gmail.com>
2018-01-22 15:51:26 +0800

    Add functionalities of arrows and images

--- script.js
+++ script.js
@@ -1,8 +1,64 @@
 $(document).ready(function(){
+
+  var current_li;
+
   $("#portfolio img").click(function(){
     var src = $(this).attr("src");
-    $("#frame img").attr("src",src);  //"src" should be equal with the src variable
+
+    //get the parent of the images, which is the current image
+    current_li = $(this).parent();
+
+    //"src" should be equal with the src variable
+    $("#main").attr("src",src);
     $("#frame").fadeIn();
     $("#overlay").fadeIn();
+  });
+
+  $("#overlay").click(function(){
+    $(this).fadeOut();
+    $("#frame").fadeOut();
+  });
+
+  $("#right").click(function(){
+
+    var next_li;
+
+    //if the image is the last one, go back to the first image
+    if(current_li.is(":last-child")){
+      next_li = $("#portfolio li").first();
+    }else{
+      //the next image of the parent image
+      next_li = current_li.next();
+    }
+
+    //get the children of next image, which is img and fetch the src using attribute
+    var next_src = next_li.children("img").attr("src");
+    $("#main").attr("src",next_src);
+    current_li = next_li;
+  });
+
+  $("#left").click(function(){
+
+    var prev_li;
+
+    if(current_li.is(":first-child")){
+      prev_li = $("#portfolio li").last();
+    }else{
+      //the previous image of the parent image
+      prev_li = current_li.prev();
+    }
+
+    //get the children of previous image, which is img and fetch the src using attribute
+    var prev_src = prev_li.children("img").attr("src");
+    $("#main").attr("src",prev_src);
+    current_li = prev_li;
+  });
+
+  $("#right, #left").mouseover(function(){
+    $(this).css("opacity","1.0");
+  });
+
+  $("#right, #left").mouseleave(function(){
+    $(this).css("opacity","0.75");
   });
 });
