e4680da6ec909e95714be544cbff550ffbb480d3
Mary Chew <jiayi1172@gmail.com>
2018-01-24 23:48:15 +0800

    AJAX

---
+++ AJAX/server.php
@@ -0,0 +1,21 @@
+<?php
+  if(isset($_POST["password"])){
+    $password = $_POST["password"];
+    $login = $_POST["login"];
+
+    $database = file_get_contents("database.txt");
+
+    $pairs = explode("-",$database);
+
+    for($i=0;$i<3;$i++){
+      $user = explode(":",$pairs[$i]);
+      if(($user[0]==$login)&&($user[1]==$password)){
+        echo "Welcome, ".$user[0]."<br><h1>Welcome to the Members Area</h1>";
+        $loggedin = 1;
+      }
+    }
+    if($loggedin!=1){
+      echo "Wrong login";
+    }
+  }
+?>
